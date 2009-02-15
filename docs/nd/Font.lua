08b4a00666ce2baffe043b68c7a87d51e7564b8f
Anders Andersson <trezker@gmail.com>
2009-02-15 17:08:38 +0100

    Adding font docs

---
+++ docs/nd/Font.lua
@@ -0,0 +1,28 @@
+-- Class: Font
+
+-- Function: load_ttf
+-- Parameters:
+-- filename - Path to ttf font
+-- size - Size
+-- options - Options
+--
+-- Returns:
+-- Font
+function load_ttf (filename, size, options) end
+
+-- Function: load_image
+-- Parameters:
+-- filename - Path to ttf font
+--
+-- Returns:
+-- Font
+function load_image (filename) end
+
+-- Function: textout
+-- Parameters:
+-- font - Font to render with
+-- x - Horizontal position
+-- y - Vertical position
+-- text - The text
+-- chars - Number of characters to actually render
+function textout (font, x, y, text, chars) end
