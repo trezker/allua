fea233cf92c6b15d811056b3a233a299c93d1907
Anders Andersson <trezker@gmail.com>
2009-02-15 09:09:41 +0100

    Adding Color docs

---
+++ docs/nd/Color.lua
@@ -0,0 +1,19 @@
+-- Class: Color
+
+-- Function: map_rgba
+-- Parameters:
+-- r - red component
+-- g - green component
+-- b - blue component
+-- a - alpha component
+--
+-- Returns:
+-- A color mapped from the input
+function map_rgba (r, g, b, a) end
+
+-- Function: put_pixel
+-- Parameters:
+-- x - Horizontal position
+-- y - Vertical position
+-- color - Color
+function put_pixel (x, y, color) end
