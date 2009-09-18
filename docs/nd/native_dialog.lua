2e1e045cc6f565e3c4ee53f1234a7c66c35713e1
Anders Andersson <trezker@gmail.com>
2009-09-18 15:42:30 +0200

    file chooser show

--- docs/nd/native_dialog.lua
+++ docs/nd/native_dialog.lua
@@ -6,3 +6,10 @@
 -- Returns:
 -- native_dialog on success, nil on failure
 function create (initial_path, title, patterns, mode) end
+
+-- Function: show
+-- Parameters:
+--
+-- Returns:
+-- 
+function show (native_dialog) end
