7dd6417e4cec4927c1fe66d0f48fd7ed57839b20
Anders Andersson <trezker@gmail.com>
2010-07-29 11:13:38 +0200

    Fixed mixer test of stream attachment

--- test/mixer.lua
+++ test/mixer.lua
@@ -4,17 +4,13 @@ USE_EXPECTED_ACTUAL_IN_ASSERT_EQUALS = false
 assertEqualsDelta = function(expected, actual, delta)
 	assert(math.abs(expected-actual)<delta)
 end
-allegro5.init()
-superdisplay = allegro5.display.create(800, 600)
-
 
 Test_mixer = {}
 
 function Test_mixer:test01_prepare()
---	allegro5.init()
-	allegro5.audio.install(allegro5.audio.AUDIO_DRIVER_AUTODETECT);
+	allegro5.init()
+	allegro5.audio.install();
 	allegro5.audio.init_acodec_addon();
-	allegro5.audio.reserve_samples(2);
 end
 
 function Test_mixer:test02_create()
@@ -22,6 +18,7 @@ function Test_mixer:test02_create()
 	assertEquals("mixer", tostring(mixer):sub(1, 5))
 end
 
+--[[
 function Test_mixer:test03_default()
 	default_mixer = allegro5.mixer.get_default ()
 	b = default_mixer:set_default()
@@ -30,8 +27,11 @@ function Test_mixer:test03_default()
 	assertEquals("boolean", type(b))
 --	assertEquals("boolean", type(r))
 end
-
+--]]
 function Test_mixer:test04_attach()
+	voice = allegro5.voice.create (44100, allegro5.audio.AUDIO_DEPTH_FLOAT32, allegro5.audio.CHANNEL_CONF_2)
+	mixer:attach_voice(voice)
+
 	mixer2 = allegro5.mixer.create (44100, allegro5.audio.AUDIO_DEPTH_FLOAT32, allegro5.audio.CHANNEL_CONF_2)
 	mb = mixer:attach_mixer(mixer2)
 
@@ -39,7 +39,8 @@ function Test_mixer:test04_attach()
 	sample_instance = sample:create_instance()
 	sampleb = mixer:attach_sample(sample_instance)
 
-	stream = allegro5.audio_stream.load("data/powerup.ogg", 2, 2)
+	stream = allegro5.audio_stream.load("data/powerup.ogg", 4, 2048)
+	stream:set_playmode (allegro5.audio.PLAYMODE_ONCE)
 	streamb = mixer:attach_audio_stream (stream)
 
 	ab = mixer2:get_attached()
@@ -85,11 +86,11 @@ end
 
 function Test_mixer:test20_destroy()
 	sample_instance = nil
-	collectgarbage()
+	sample = nil
+	stream = nil
 	mixer = nil
 	mixer2 = nil
-	stream = nil
-	sample = nil
+	collectgarbage()
 	collectgarbage()
 end
 
