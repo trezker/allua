e83292e4007af905efab20d745c80c4da0c1b1bc
Anders Andersson <trezker@gmail.com>
2010-09-01 13:40:10 +0200

    Adding fs_entry docs

---
+++ docs/nd/Fs_entry.lua
@@ -0,0 +1,61 @@
+-- Class: fs_entry
+
+-- Function: create
+-- Parameters:
+function create (path) end
+
+-- Function: get_name
+-- Parameters:
+function get_name (path) end
+
+-- Function: update
+-- Parameters:
+function update (path) end
+
+-- Function: get_mode
+-- Parameters:
+function get_mode (path) end
+
+-- Function: get_atime
+-- Parameters:
+function get_atime (path) end
+
+-- Function: get_ctime
+-- Parameters:
+function get_ctime (path) end
+
+-- Function: get_mtime
+-- Parameters:
+function get_mtime (path) end
+
+-- Function: get_size
+-- Parameters:
+function get_size (path) end
+
+-- Function: exists
+-- Parameters:
+function exists (path) end
+
+-- Function: is_file
+-- Parameters:
+function is_file (path) end
+
+-- Function: is_directory
+-- Parameters:
+function is_directory (path) end
+
+-- Function: remove
+-- Parameters:
+function remove (path) end
+
+-- Function: open_directory
+-- Parameters:
+function open_directory (path) end
+
+-- Function: close_directory
+-- Parameters:
+function close_directory (path) end
+
+-- Function: read_directory
+-- Parameters:
+function read_directory (path) end
