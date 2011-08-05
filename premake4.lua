d4006c5ca31ffba1684ba7af16f72064645f931b
Luiji Maryo <luiji@users.sourceforge.net>
2011-08-05 16:31:54 -0400

    cleaned up premake4.lua

--- premake4.lua
+++ premake4.lua
@@ -1,43 +1,50 @@
-lib_name = "allua"
-dependencies = { "allegro", "allegro_image", "allegro_font", "allegro_ttf", "allegro_primitives", "allegro_audio", "allegro_acodec", "allegro_dialog" }
+NAME = "allua"
 
-solution (lib_name)
-	configurations { "Debug", "Release" }
+solution (NAME)
+  configurations { "Debug", "Release" }
 
-	project (lib_name)
-		kind "SharedLib"
-		language "C"
-		location "build"
-		files { "src/*.c" }
-		targetdir "build/lib"
-		includedirs { "../include" }
-		links (dependencies)
-		flags { "ExtraWarnings", "FatalWarnings" }
---		postbuildcommands { "cp lib/liballua.so ../test/liballua.so", "cd ../test && lua unittest_main.lua" }
+  project (NAME)
+    kind "SharedLib"
+    language "C"
+    files {"src/**.c", "include/allua/**.h" }
+    includedirs "include"
+    flags {"ExtraWarnings", --[["FatalWarnings"]]}
 
-if _ACTION == "clean" then
-	os.rmdir "build"
-end
-		
+    links
+    {
+      "allegro",
+      "allegro_image",
+      "allegro_font",
+      "allegro_ttf",
+      "allegro_primitives",
+      "allegro_audio",
+      "allegro_acodec",
+      "allegro_dialog"
+    }
+
+--  configuration { "linux", "gmake" }
+--    -- Turn GCC into a C89-loving maniac
+--    buildoptions { "-ansi", "-pedantic" }
+
 newaction {
-	trigger     = "test",
-	description = "Run tests",
+  trigger     = "test",
+  description = "Run tests",
 
-	execute = function ()
-		os.execute ("cp lib/liballua.so ../test/liballua.so")
-		if not _OPTIONS["test"] then
-			tests = os.matchfiles("test/*.lua")
-			for index, name in pairs(tests) do
-				os.execute ("lua " .. name)
-				cmd = io.stdin:read'*l'
-				if cmd == "q" then
-					break;
-				end
-			end
-		else
-			os.execute ("lua test/" .. _OPTIONS["test"] .. ".lua")
-		end
-	end
+  execute = function ()
+    os.execute ("cp lib/liballua.so ../test/liballua.so")
+    if not _OPTIONS["test"] then
+      tests = os.matchfiles("test/*.lua")
+      for index, name in pairs(tests) do
+        os.execute ("lua " .. name)
+        cmd = io.stdin:read'*l'
+        if cmd == "q" then
+          break;
+        end
+      end
+    else
+      os.execute ("lua test/" .. _OPTIONS["test"] .. ".lua")
+    end
+  end
 }
 
 newoption {
@@ -53,36 +60,36 @@ newoption {
 }
 
 newaction {
-	trigger     = "install",
-	description = "Install the software",
-	execute = function ()
-		-- copy files, etc. here
-		os.mkdir(_OPTIONS["dir"].."lib/");
-		files = os.matchfiles("build/lib/*")
-		print ("Installing lib files to " .. _OPTIONS["dir"] .."lib/")
-		for k, f in pairs(files) do
-			print ("Copying " .. f)
-			os.copyfile(f, _OPTIONS["dir"].."lib/")
-			if _OPTIONS["dir"] == "/usr/local/" then
-				os.copyfile(f, "/usr/local/lib/lua/5.1/")
-			end
-		end
-		os.mkdir(_OPTIONS["dir"].."include/"..lib_name.."/");
-		files = os.matchfiles(lib_name.."/*")
-		print ("Installing header files to " .. _OPTIONS["dir"] .."include/")
-		for k, f in pairs(files) do
-			print ("Copying " .. f)
-			os.copyfile(f, _OPTIONS["dir"].."include/"..lib_name.."/")
-		end
-	end
+  trigger     = "install",
+  description = "Install the software",
+  execute = function ()
+    -- copy files, etc. here
+    os.mkdir(_OPTIONS["dir"].."lib/");
+    files = os.matchfiles("build/lib/*")
+    print ("Installing lib files to " .. _OPTIONS["dir"] .."lib/")
+    for k, f in pairs(files) do
+      print ("Copying " .. f)
+      os.copyfile(f, _OPTIONS["dir"].."lib/")
+      if _OPTIONS["dir"] == "/usr/local/" then
+        os.copyfile(f, "/usr/local/lib/lua/5.1/")
+      end
+    end
+    os.mkdir(_OPTIONS["dir"].."include/"..NAME.."/");
+    files = os.matchfiles(NAME.."/*")
+    print ("Installing header files to " .. _OPTIONS["dir"] .."include/")
+    for k, f in pairs(files) do
+      print ("Copying " .. f)
+      os.copyfile(f, _OPTIONS["dir"].."include/"..NAME.."/")
+    end
+  end
 }
 
 newaction {
-	trigger     = "docs",
-	description = "Build documentation",
-	execute = function ()
-		os.execute("../NaturalDocs/NaturalDocs -i include -i src -i test -i docs/nd -o HTML docs/html -p nd -ro")
-	end
+  trigger     = "docs",
+  description = "Build documentation",
+  execute = function ()
+    os.execute("../NaturalDocs/NaturalDocs -i include -i src -i test -i docs/nd -o HTML docs/html -p nd -ro")
+  end
 }
 
 if not _OPTIONS["dir"] then
@@ -90,5 +97,7 @@ if not _OPTIONS["dir"] then
 end
 
 if not ("/" == _OPTIONS["dir"]:sub(_OPTIONS["dir"]:len())) then
-	_OPTIONS["dir"] = _OPTIONS["dir"] .. "/"
+  _OPTIONS["dir"] = _OPTIONS["dir"] .. "/"
 end
+
+-- vim:set ts=8 sts=2 sw=2 noet:
